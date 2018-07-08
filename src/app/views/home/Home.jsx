9abeb2b34161a3647ec977da8b9bfbbca00da60d
Zach Kelling <z@zeekay.io>
2018-07-08 14:24:15 -0400

    Update language

--- src/app/views/home/Home.jsx
+++ src/app/views/home/Home.jsx
@@ -4,13 +4,16 @@ import {Link} from 'react-router-dom'
 export const Home = (props) => (
   <div className="pure-g">
     <div className="pure-u-1-1">
-      <p>
-        Are you an investor with Shopin? Check with your investor group for a unique url for your group. If you're unsure who that is, <a href="mailto:support@shopin.com">contact us</a>!
+      <p>/
+        Welcome to the Shopin Token Swap Dapp. Check with your syndicate lead
+        for a unique link to register interest in the Shopin token swap. If you're
+        unsure who that is, <a href="mailto:support@shopin.com">contact us</a>!
       </p>
     </div>
     <div className="pure-u-1-1">
       <p>
-        If you're a syndicate and you have not received token swap contract of your own, head over to the syndicate page: <Link to="/syndicate">/syndicate</Link>.
+        If you're a syndicate and you have not received token swap contract of
+        your own, head over to the syndicate page: <Link to="/syndicate">/syndicate</Link>.
       </p>
     </div>
   </div>
