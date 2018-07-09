47f2b5a3b0e053d9c5a67ec0a5c6ca7929725f0f
Zach Kelling <z@zeekay.io>
2018-07-09 14:25:46 -0400

    Remove csv require

--- migrations/3_transfer_contract.js
+++ migrations/3_transfer_contract.js
@@ -1,4 +1,3 @@
-const csv = require ('csv')
 const logger = require ('../lib/logger')
 const ShopinToken = artifacts.require ('ShopinToken')
 const OriginalToken = artifacts.require('OriginalToken')
