ca2d434b59483ecd8175d51d3e79b5c41334963f
Zach Kelling <z@zeekay.io>
2018-07-09 12:31:05 -0400

    Update truffle config

--- truffle.js
+++ truffle.js
@@ -1,4 +1,36 @@
+require('babel-register');
+require('babel-polyfill');
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const secrets = require('./secrets')
+
+const mnemonic = process.env.MNEMONIC || secrets.mnemonic;
+const infura = process.env.INFURA || secrets.infura
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*'
+    },
+    production: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `https://mainnet.infura.io/${infura}`, 0)
+      },
+      gas: 4700000,
+      network_id: 1
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infura}`, 0)
+      },
+      network_id: 3
+    },
+  },
+  mocha: {
+    reporter: 'spec'
+  }
 };
