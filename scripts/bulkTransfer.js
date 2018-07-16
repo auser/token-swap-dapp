95c18378fe146e81d1df62b5ae8c54d12afe86e1
Ari <ari@fullstack.io>
2018-07-16 19:21:01 -0400

    Set the network (default to development)

--- scripts/bulkTransfer.js
+++ scripts/bulkTransfer.js
@@ -1,55 +1,78 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const path = require('path')
-const truffleContract = require('truffle-contract')
-const Web3 = require('web3')
-const BigNumber = require('bignumber.js')
-const csvjson = require('csvjson')
+const fs = require ('fs');
+const path = require ('path');
+const truffleContract = require ('truffle-contract');
+const Web3 = require ('web3');
+const BigNumber = require ('bignumber.js');
+const csvjson = require ('csvjson');
 
-const contractsDir = path.join(path.resolve(__dirname), '..', 'build', 'contracts')
+const contractsDir = path.join (
+  path.resolve (__dirname),
+  '..',
+  'build',
+  'contracts'
+);
 
-const ShopinToken = require(path.join(contractsDir, 'ShopinToken.json'))
+const ShopinToken = require (path.join (contractsDir, 'ShopinToken.json'));
 
-const contract = truffleContract(ShopinToken)
-const provider = new Web3.providers.HttpProvider("http://localhost:8545");
-contract.setProvider(provider)
+let secrets = {};
+try {
+  secrets = require ('./secrets');
+} catch (err) {}
 
-const argv = require('yargs')
-.option('csvfile', {
-  alias: 'f',
-  demandOption: true,
-  describe: 'csv file ([address], [token])'
-})
-.option('owner', {
-  alias: 'o',
-  demandOption: true,
-  describe: 'owner account'
-})
-.option('tokenAddress', {
-  alias: 't',
-  demandOption: true,
-  describe: 'token address'
-})
-.help()
-  .argv
+const INFURA = process.env.INFURA || secrets.infura;
 
-const owner = argv.owner
-const data = fs.readFileSync(argv.csvfile, {encoding: 'utf-8'})
-const opts = {}
-const investorArr = csvjson.toObject(data, opts)
+const networks = {
+  development: 'http://localhost:8545',
+  ropsten: `https://ropsten.infura.io/${INFURA}`,
+  mainnet: `https://mainnet.infura.io/${INFURA}`,
+};
 
-const calculateAmount = num => (new BigNumber(num))* (10 ** 18)
+const contract = truffleContract (ShopinToken);
+const argv = require ('yargs')
+  .option ('csvfile', {
+    alias: 'f',
+    demandOption: true,
+    describe: 'csv file ([address], [token])',
+  })
+  .option ('owner', {
+    alias: 'o',
+    demandOption: true,
+    describe: 'owner account',
+  })
+  .option ('tokenAddress', {
+    alias: 't',
+    demandOption: true,
+    describe: 'token address',
+  })
+  .option ('network', {
+    alias: 'n',
+    describe: 'network to run on',
+    default: 'development',
+  })
+  .help ().argv;
 
-contract.at(argv.tokenAddress).then(async instance => {
-  const res = await Promise.all(
-    investorArr.map(async obj => {
-      const addr = obj['address']
-      const tokens = calculateAmount(obj['amount'])
-      return await instance.transfer(obj['address'], tokens, {from: owner})
-    }))
+const provider = new Web3.providers.HttpProvider (networks[argv.network]);
+contract.setProvider (provider);
 
-  console.log('res ->', res)
-})
+const owner = argv.owner;
+const data = fs.readFileSync (argv.csvfile, {encoding: 'utf-8'});
+const opts = {};
+const investorArr = csvjson.toObject (data, opts);
+
+const calculateAmount = num => new BigNumber (num) * 10 ** 18;
+
+contract.at (argv.tokenAddress).then (async instance => {
+  const res = await Promise.all (
+    investorArr.map (async obj => {
+      const addr = obj['address'];
+      const tokens = calculateAmount (obj['amount']);
+      return await instance.transfer (obj['address'], tokens, {from: owner});
+    })
+  );
+
+  console.log ('res ->', res);
+});
 
 //contract.setProvider(provider)
